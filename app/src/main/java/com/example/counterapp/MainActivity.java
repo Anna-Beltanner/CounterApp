e23f7fcb5b14859755ae514c370fd30b6b36557c
Anna <anuta.piter@gmail.com>
2020-01-27 23:07:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.counterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    MainActivityViewModel model;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        model = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+
+        textView = findViewById(R.id.textView);
+
+        LiveData<Integer> countLiveData = model.getCurrentValue();
+
+        countLiveData.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                textView.setText(String.valueOf(integer));
+            }
+        });
+
+
+    }
+
+    public void decreaseValue(View view) {
+
+        model.getDecreasedValue();
+    }
+
+    public void increaseValue(View view) {
+        model.getIncreasedValue();
+    }
+}
+
